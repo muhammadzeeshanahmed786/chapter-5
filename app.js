05d06fe227cd2f9e2ee8503c6a40051f6e746316
Muhammad Zeeshan Ahmed <78692718+shani2103792@users.noreply.github.com>
2021-09-21 08:36:23 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,267 @@
+// 1
+
+
+// Addition
+// var first=+prompt("Enter First Number");
+// var second=+prompt("Enter second Number");
+
+// console.log(first + second);
+
+
+// 2
+
+
+// subtaction
+// var first=+prompt("Enter First Number");
+// var second=+prompt("Enter second Number");
+
+// console.log(first - second);
+
+
+// // Multiplication
+// var first=+prompt("Enter First Number");
+// var second=+prompt("Enter second Number");
+
+// console.log(first * second);
+
+// Division
+
+// var first=+prompt("Enter First Number");
+// var second=+prompt("Enter second Number");
+
+// console.log(first / second);
+
+
+
+
+// 3
+
+
+// var value=5;
+// document.write("Initial Value: " + value);
+// document.write("<br>")
+// document.write("After Incriment The Value is: " + ++value)
+// document.write("<br>");
+// var val=value+7;
+// document.write( " Value After Addition " + val);
+// document.write("<br>");
+// var dec=--val;
+
+// document.write("Value After Decrement " + dec)
+// document.write("<br>");
+
+// document.write("The Reminder is " + dec%3)  
+
+
+
+
+
+// 4
+
+
+// var price=600;
+// var tickets=5;
+// var totalCost=600*5;
+// document.write("Total Cost Of 5 Tickets to a movie is " + totalCost + " PKR")
+
+
+
+
+
+// 5
+
+
+
+// var tableNumber=+prompt("Enter Table Number");
+// document.write("Table Of  " + tableNumber + "<br>")
+// for(i=1; i<=10; i++){
+//     document.write(tableNumber + " X " + i + " = " + tableNumber*i + "<br>")
+// }
+
+
+
+
+
+// 6
+
+
+
+// var celcius=+prompt("Celsius value")
+// var c =(celcius*9/5)+32
+// document.write(celcius+"C " + "is "+ c+"F")
+// document.write("<Br>")
+// var farnheit=+prompt("Fahrenheit value")
+// var f =(farnheit-32)*5/9
+
+// document.write(farnheit+"F " + "is "+f+"C" )
+
+
+
+
+
+
+
+// 7
+
+
+
+// document.write("<h1>"+"E-Commerce"+"</h1>")
+
+// var price1=+prompt("Price of item 1")
+// document.write("Price of item 1 is "+price1)
+// document.write("<Br>")
+
+// var quantity1=+prompt("Quantity of item 1")
+// document.write("Quantity of item 1 is "+quantity1)
+// document.write("<Br>")
+
+// var price2=+prompt("Price of item 2")
+// document.write("Price of item 2 is "+price2);
+// document.write("<Br>")
+
+
+
+// var quantity2=+prompt("Quantity of item 2")
+// document.write("Quantity of item 2 is "+quantity2)
+// document.write("<Br>")
+// document.write("Shipping Charge 100")
+// document.write("<Br>")
+
+// var product=price1*quantity1;
+// var product2=price2*quantity2;
+
+// var sum=product+product2+100
+// document.write("Total Cost Of Your Order "+sum)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// 8
+
+
+
+
+// document.write("<h1>"+"Mark Sheet"+"</h1>")
+
+// var obtained=+prompt("Enter Obtained Marks")
+// document.write("Otained Marks "+ obtained)
+// document.write("<Br>")
+
+// var total=+prompt("Enter Total Marks")
+// document.write("Total Marks "+total)
+// document.write("<Br>")
+
+// var per=obtained*100/total;
+// var p =per.toFixed(3)
+// document.write("Percentage"+p+"%")
+
+
+
+
+
+
+// 9
+
+
+
+
+// var us=+prompt("Enter US Curreny")
+// var po=us*104;
+// var saudiReal=+prompt("Enter Saudi Reyal")
+// var so=saudiReal*28
+// document.write(us + " US Dollar =  "+ po+ " Palistani Rupee and "+ saudiReal + " Saudi Reyal ="+ so +" Pakistani Rupee")
+
+
+
+// 10
+
+
+
+// var add=5*10/2;
+// alert(add)
+
+
+
+// 11
+
+
+
+// var age=+prompt("Enter Current Year")
+// var birthYear=+prompt("Enter Birth Year ")
+
+// document.write("Current Year :" + age)
+// document.write("<br>" )
+// document.write("Birth Year  :" + birthYear)
+// document.write("<br>" )
+// var sum=age-birthYear;
+// document.write("your Age is :" + sum)
+
+
+
+// 12
+
+
+// var radius=+prompt("Radius of Circle  ")
+// var pi=3.142;
+// document.write("Radius Of Circle :" + radius)
+// document.write("<br>")
+// var circumference=2*radius*pi;
+// document.write("The Circumfernece is :" + circumference )
+// document.write("<br>")
+
+// var area=radius*radius*pi
+
+// document.write("The Area Is :" +area)
+
+
+
+
+
+
+
+// question 13
+
+
+// var snack=prompt ("Enter favorite Snack")
+// var currentAge=+prompt("Enter Current age")
+// var maxAge=+prompt("Enter Maximum Age")
+// var estimate=+prompt("Amount of Snacks per day")
+
+// document.write("Favorite Snack :" + snack)
+// document.write("<br>")
+// document.write("Current Age : " + currentAge)
+// document.write("<br>")
+// document.write(" Estimated Maximum Age : " + maxAge)
+// document.write("<br>")
+// document.write("Amount Of Snacks Per Day : " + estimate)
+// document.write("<br>")
+
+// var sub=maxAge-currentAge*estimate
+// var products=sub*30
+// document.write("You Will Need " + products + snack +"   To Last You Until The Ripe Old Age Of " +maxAge)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
